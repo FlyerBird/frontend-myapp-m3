797dcb3d958d0b14fac3a2e446141bb71fb9445b
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-09-09 13:53:48 +0200

    organzing signUp view

--- src/views/auth/Signup.jsx
+++ src/views/auth/Signup.jsx
@@ -43,15 +43,24 @@ export default function Signup() {
   return (
     <div className='signUp'>
       <form onSubmit={handleSubmit}>
+      <div className='signUpFormSections'>
         <label>Username</label>
         <input required type="text" name="username" value={user.username} onChange={handleChange} />
+      </div>
+      <div className='signUpFormSections'>
         <label>Email</label>
         <input required type="email" name="email" value={user.email} onChange={handleChange} />
+      </div>
+      <div className='signUpFormSections'>
         <label>Password</label>
         <input required type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value) } />
+      </div>
+      <div className='signUpFormSections'>
         <label>Repeat the password</label>
         <input required type="password" name="passwordControl" value={passwordControl} onChange={(e) => setPasswordControl(e.target.value)} />
         {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      </div>
+        
         <button type="submit">Register</button>
       </form>
     </div>
