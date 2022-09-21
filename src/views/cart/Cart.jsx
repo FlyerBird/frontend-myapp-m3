607392ec15135ed47f02826e28943e89afc0474f
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-09-21 13:02:05 +0200

    CartView WIP

--- src/views/cart/Cart.jsx
+++ src/views/cart/Cart.jsx
@@ -1,23 +1,52 @@
 import React, {useEffect, useState, useContext} from 'react';
 import { AuthContext } from "../../context/AuthContext";
+import { useParams, useNavigate} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 export default function Cart() {
+  const storedToken = localStorage.getItem('authToken')
+  const { id } = useParams();
   const [products, setProducts] = useState(null);
-  const { getCart, cart } = useContext(AuthContext);
+  const { cart } = useContext(AuthContext);
+  const navigate = useNavigate();
 // de context rebras cart, useeffect lunic que fa es agafar cart i fer setproducts(cart)
 useEffect (() => {
   setProducts(cart)
 }
 ,[cart])
   // array dependencies cart
-  
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_API_URL}/product/${id}`,  { headers: { Authorization: `Bearer ${storedToken}` } });
+      toast.success('Product removed')
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <div>
+    <div className='Cart'>
+    <div className='back'>
+      <button onClick={() => navigate(-1)}><FontAwesomeIcon icon={faCaretLeft} /></button>
+    </div>
     {products && products.map((product, i) => {
-      return <div key={product._id + i}>
+      return <div className='onEachProCart' key={product._id + i}>
+      <div className='eachProductCart'>
+      <div className='cartProductImg'>
+      {Array.isArray(product.images) ? <img className='imgProductCart' src={product.images[0]} alt={`Pic of ${product.title}`} /> : <img className='imgProductCart' src={product.images} alt={`Pic of ${product.title}`} />}
+      </div>
+      <div className='cartProductTxt'>
       <p>{product.title}</p>
-      <p>{product.price}</p>
+      <p>{product.price}€</p>
+      </div>
+      <div className='cartRemoveButton'>
+      <button onClick={handleDelete}>Remove</button>
+      </div>
+      </div>
       </div>
     })}
     {!products && <p>No products</p>}
